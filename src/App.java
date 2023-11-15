7aaa3e6e09e9bde729ae7f6aa7031baabba87604
AleVallejoGarcia <alevalga@gmail.com>
2023-11-15 13:10:55 +0000

    fix: arreglo en las prioridades

--- src/App.java
+++ src/App.java
@@ -8,19 +8,18 @@ public class App {
         Productor granjero1 = new Productor(restaurante, 5, "Manolo", capacidadAlmacenamiento);
         Productor granjero2 = new Productor(restaurante, 5, "Gebrasio", capacidadAlmacenamiento);
 
-        Consumidor cliente1 = new Consumidor("Manue", restaurante, 2);
-        Consumidor cliente2 = new Consumidor("Ricardo", restaurante, 3);
-
-        granjero1.start();
-        granjero2.start();
-        cliente1.start();
-        cliente2.start();
+        Consumidor cliente1 = new Consumidor("Manue", restaurante, 5);
+        Consumidor cliente2 = new Consumidor("Ricardo", restaurante, 5);
 
         granjero1.setPriority(Thread.MAX_PRIORITY);
         granjero2.setPriority(Thread.MAX_PRIORITY);
         cliente1.setPriority(Thread.MIN_PRIORITY);
         cliente2.setPriority(Thread.MIN_PRIORITY);
 
+        granjero1.start();
+        granjero2.start();
+        cliente1.start();
+        cliente2.start();
 
         try {
             granjero1.join();
